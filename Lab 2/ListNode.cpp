827e927b3182e6db6e2fb8e20bf7a728946bbdf4
vivianma141 <65625933+vivianma141@users.noreply.github.com>
2021-03-04 21:35:46 -0500

    Add files via upload

---
+++ Lab 2/ListNode.cpp
@@ -0,0 +1,12 @@
+// Vivian Ma, vm8ufh, 2/23, ListNode.cpp
+
+#include "ListNode.h"
+#include <iostream>
+
+using namespace std;
+
+ListNode::ListNode() {
+  value = NULL;
+  next = NULL;
+  previous = NULL; 
+}
